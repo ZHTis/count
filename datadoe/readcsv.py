078f8e0117d6f2381517ba55a9668ffcdc54514c
ZHTis <2029322886@qq.com>
2024-07-16 16:26:35 +0800

    r

---
+++ datadoe/readcsv.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import ast
+
+# the data form is required to have no space at the beginning and nothing and the end,
+# and dicts should be separated with comma.
+
+path = '/Users/rubythurs/Documents/save_2023-10-14-11.36.22.csv'
+with open(path) as f:
+    data = f.read()
+    data2 = ast.literal_eval(data) #tuple
+
+dictx = []
+
+for item in data2:
+    if isinstance(item, dict):
+        dictx.append(item)
+
+print(type(dictx))
+pd.set_option('display.max_columns', 40)
+df = pd.DataFrame(dictx)
+print(df)
